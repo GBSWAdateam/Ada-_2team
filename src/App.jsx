b733f1c4587824ec07738cf0abfadf3fecbeea80
rjsgud49 <162693556+rjsgud49@users.noreply.github.com>
2024-11-14 10:31:07 +0900

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import Description from './pages/Description';
-import SavePath from './pages/SavePath';
-import Main from './pages/Main';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import LoginForm from "./pages/login";
+import Description from "./pages/Description";
+import SavePath from "./pages/SavePath";
+import Main from "./pages/login";
+
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Main/>} />
-      <Route path="zigtongDesc" element={<Description/>} />
-      <Route path='savePath' element={<SavePath />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="zigtongDesc" element={<Description />} />
+            <Route path="savePath" element={<SavePath />} />
+        </Routes>
+    );
 }
 
 export default App;
---
+++ src/pages/login.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./login.css";
+
+// Ionicon 사용을 위해 React Icon 패키지나 라이브러리를 설치해 사용하는 방법이 있을 수 있습니다.
+// 예를 들어 react-icons와 같은 패키지를 사용할 수도 있습니다.
+
+const LoginForm = () => {
+    return (
+        <section>
+            <div className="third"></div>
+            <div className="fourth"></div>
+
+            <div className="form-box">
+                <div className="form-value">
+                    <form action="">
+                        <h2>Login</h2>
+                        <div className="inputbox">
+                            <ion-icon name="mail-outline"></ion-icon>
+                            <input type="email" required />
+                            <label>Email</label>
+                        </div>
+                        <div className="inputbox">
+                            <ion-icon name="lock-closed-outline"></ion-icon>
+                            <input type="password" required />
+                            <label>Password</label>
+                        </div>
+                        <div className="forget">
+                            <label>
+                                <input type="checkbox" /> Remember me
+                            </label>
+                            <label>
+                                <a href="#">Forgot password?</a>
+                            </label>
+                        </div>
+                        <button type="submit">Log in</button>
+                        <div className="register">
+                            <p>
+                                Don't have an account? <a href="#">Register</a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default LoginForm;
