5cc50e3ea7a62bfcbdb22d91da4013154946c2b4
rjsgud49 <162693556+rjsgud49@users.noreply.github.com>
2024-11-15 16:54:37 +0900

    Modify App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import LoginForm from "./pages/login";
 import Signup from "./pages/signup";
 import Description from "./pages/Description";
 import SavePath from "./pages/SavePath";
-import Main from "./pages/login";
+import Main from "./pages/Main";
 
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
@@ -11,7 +11,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
     return (
         <div>
-            <LoginForm />
+            <Main />
         </div>
     );
 }
