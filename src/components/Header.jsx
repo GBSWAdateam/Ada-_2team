6ad10ecb4e8769d1ad45c8f9892d203a77fd2bdd
rjsgud49 <162693556+rjsgud49@users.noreply.github.com>
2024-11-30 10:03:34 +0900

    로고 클릭시 메인페이지로 이동

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,36 @@
-import './Header.css';
-import Button from './Button';
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import "./Header.css";
+import Button from "./Button";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
     const navigate = useNavigate();
-    return(
+    return (
         <div className="Header">
-            <img src="/images/zigtong_logo.png" alt="zigtong 로고" style={{width:'130px'}}/>
+            <Link to="/" className="Header_link">
+                <img
+                    src="/images/zigtong_logo.png"
+                    alt="zigtong 로고"
+                    style={{ width: "130px" }}
+                />
+            </Link>
             <div className="Header_navi">
-                <Link to="/zigtongDesc" className='Header_link'>ZigTong 설명</Link>
-                <Link to="/savePath" className='Header_link'>저장한 경로 보기</Link>
-                <Button text="trip planning" onclick={()=>{navigate('')}} type={'blueButton'}/>
+                <Link to="/zigtongDesc" className="Header_link">
+                    ZigTong 설명
+                </Link>
+                <Link to="/savePath" className="Header_link">
+                    저장한 경로 보기
+                </Link>
+                <Button
+                    text="trip planning"
+                    onclick={() => {
+                        navigate("");
+                    }}
+                    type={"blueButton"}
+                />
             </div>
-            
         </div>
-    )
-}
+    );
+};
 
 export default Header;
